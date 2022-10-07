0347a3b062d5af5eef70c6d5cda2cc00edacf603
Anthony Rotella <mr4406@yahoo.com>
2022-10-06 20:32:38 -0500

    thursday

--- frontend/src/component/home/navbar.js
+++ frontend/src/component/home/navbar.js
@@ -7,7 +7,7 @@ import { Container } from 'react-bootstrap'
 function NavBar() {
 
     return (
-        <Navbar bg="success" variant="#b5ab3c" sticky='top' >
+        <Navbar bg="info" sticky='top' >
             <Container>
                 {/* <Navbar.Brand href="#home" style={{ 'color': 'pink' }}>Oma's Tree</Navbar.Brand> */}
                 <Navbar.Toggle />
